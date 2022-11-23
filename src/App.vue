2f0494b74997df013a76273130bdbac400917446
lzy-Jerry <763626908@qq.com>
2022-11-23 23:28:13 +0800

    test: test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+const a = "1212";
+console.log(a);
 </script>
 
 <template>
