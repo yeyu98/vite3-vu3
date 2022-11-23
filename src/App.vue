5b30ed04e0880264e6a3911343aeb3d7b88667a5
lzy-Jerry <763626908@qq.com>
2022-11-23 23:11:29 +0800

    test: 测试一下

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+const a = 1
+console.log(a)
 </script>
 
 <template>
