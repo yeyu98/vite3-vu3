5a95499768b492c12f194b91749b5b460c403656
lzy-Jerry <763626908@qq.com>
2022-11-25 20:34:40 +0800

    feat: eslint配置驼峰

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'airbnb-base',
-    'plugin:prettier/recommended' // 添加 prettier 插件,
+    "plugin:vue/vue3-essential",
+    "airbnb-base",
+    "plugin:prettier/recommended", // 添加 prettier 插件,
   ],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['vue'],
-  rules: {}
-}
+  plugins: ["vue"],
+  rules: {
+    // 强制驼峰写法
+    camelcase: "warn",
+  },
+};
